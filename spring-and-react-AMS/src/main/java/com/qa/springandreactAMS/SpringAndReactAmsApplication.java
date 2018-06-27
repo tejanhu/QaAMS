a1f4989545acc2e3de0d2e9aabed761a77998600
Hussein Ahmed Tejan <tejanahmedhu@gmail.com>
2018-06-27 17:12:24 +0100

    first commit message

---
+++ spring-and-react-AMS/src/main/java/com/qa/springandreactAMS/SpringAndReactAmsApplication.java
@@ -0,0 +1,12 @@
+package com.qa.springandreactAMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndReactAmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndReactAmsApplication.class, args);
+	}
+}
---
+++ spring-and-react-AMS/src/test/java/com/qa/springandreactAMS/SpringAndReactAmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qa.springandreactAMS;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAndReactAmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
