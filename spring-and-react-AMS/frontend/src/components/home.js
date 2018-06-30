fdb82817ef974457ac7a1f79bb661313cef2d47e
Hussein <tejanahmedhu@gmail.com>
2018-06-30 19:09:00 +0100

    created login inputs (username, password) for homepage

--- spring-and-react-AMS/frontend/src/components/home.js
+++ spring-and-react-AMS/frontend/src/components/home.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 class Home extends Component{
     render(){
         return (
-            <div><h1>Home</h1></div>
+           
+                <form ref="userForm">
+                        <div class="container">
+                            <label for="uname"><br/>Username:<br/></label>
+                            <input type="text" class="form-control" id="uname" ref="uname" placeholder="Enter Username" required/>
+                            <label for="psw"><br/>Password:<br/></label>
+                            <input type="password" class="form-control" id="psw" ref="psw" placeholder="Enter Password" required/>                         
+                            <button type="submit" class="btn btn-primary" role="button" id="login-btn" onClick={this.addAccount}>Login</button>
+                         </div>
+                </form>
+            
         )
     }
 }
