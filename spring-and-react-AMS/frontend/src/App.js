f513eb2bef5f2e1e223a5473ff4e42ca10ab78cf
Hussein <tejanahmedhu@gmail.com>
2018-06-30 01:37:48 +0100

    testing react binding and button onClick event

--- spring-and-react-AMS/frontend/src/App.js
+++ spring-and-react-AMS/frontend/src/App.js
@@ -10,6 +10,37 @@ import { Jumbotron } from 'react-bootstrap';
 
 class App extends Component {
 
+
+  addAccount(event) {
+    event.preventDefault();
+    let name = this.refs.name.value;
+    let surname = this.refs.surname.value;
+    let accno = this.refs.accno.value;
+
+    let account = {
+      name, 
+      surname,
+      accno
+    };
+
+    let accounts = this.state.accounts;
+
+    accounts.push(account);
+
+    this.setState({
+      accounts: accounts
+    })
+  }
+
+  constructor(){
+    super();
+    this.addAccount = this.addAccount.bind(this)
+    this.state = {
+      accounts: [],
+      title: "Welcome to AMS"
+    }
+  }
+
   state = {};
 
   componentDidMount() {
@@ -25,9 +56,10 @@ class App extends Component {
   // };
 
   render() {
+    let title = this.state.title;
+    let accounts = this.state.accounts;
     return (
       <div className="App">
-        {/* <header className="App-header"> */}
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <a class="navbar-brand" href="#">AMS</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -36,13 +68,13 @@ class App extends Component {
     
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Dashboard </a>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Dashboard <span class="sr-only">(current)</span></a>
           </li>
           {/* <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
           </li> */}
-          <li class="nav-item active dropdown ">
+          <li class="nav-item dropdown ">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Account <span class="sr-only">(current)</span>
             </a>
@@ -66,7 +98,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p> */}
         <div class="jumbotron">
-          <h1 class="display-3">Welcome to AMS</h1>
+        <h1 class="display-3">{title}</h1>
           <p class="lead">This is a simple account management system.</p>
           <hr class="my-4"></hr>
           <p>It allows users to create an account, view their account, update their account and save any changes on their account.</p>
@@ -76,18 +108,21 @@ class App extends Component {
             <form>
             <div class="input-group">
             <label for="fname">First-name:</label><br/>
-            <input type="text" class="form-control" id="fname" placeholder="Enter firstname" />
+            <input type="text" class="form-control" id="fname" ref="name" placeholder="Enter firstname" />
             </div>
             <div class="input-group">
             <label for="sname">Surname:</label><br/>
-            <input type="text" class="form-control" id="lname" placeholder="Enter surname"/>
+            <input type="text" class="form-control" id="lname" ref="surname" placeholder="Enter surname"/>
             </div>
             <div class="input-group">
             <label for="accno">Account number</label><br/>
-            <input type="text" class="form-control" id="acc_no" placeholder="Enter account number"/>
+            <input type="text" class="form-control" id="acc_no" ref="accno" placeholder="Enter account number"/>
             </div>
-            <button type="submit" class="btn btn-primary" role="button" id="submit-btn">Submit</button>
+            <button type="submit" class="btn btn-primary" role="button" id="submit-btn" onClick={this.addAccount}>Submit</button>
           </form>
+          <pre>
+            {JSON.stringify(accounts)}
+          </pre>
         </div>
           </div>
       </div>
